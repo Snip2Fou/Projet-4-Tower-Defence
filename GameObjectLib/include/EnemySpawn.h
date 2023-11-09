46ba4bbff80868d277913521810c0be36e3e0687
unknown <agathelier@gamingcampus.local>
2023-11-09 15:50:01 +0100

    Editeur de vague complet

--- GameObjectLib/include/EnemySpawn.h
+++ GameObjectLib/include/EnemySpawn.h
@@ -1,19 +1,21 @@
 #pragma once
 class EnemySpawn {
 private:
-    int vaguemaxEnnemi = 5; //Ennemi max vague 1
+    int vaguemaxEnnemi = 7; //Ennemi max vague 1
     int nbEnnemiVague = 0;
-    int nbVague = 1;
     double coeffMultiplicateurEnnemi = 1.25;
+    int nbVague = 1;
     int nbVagueMax = 30;
+    bool verifVagueBoss;
 
     //Nombre à mettre en seconde
-    int cooldownBTWwaves = 8;
+    double cooldownBTWwaves = 8;
+    double cooldown = 1.2;
 
     // L'addition de tous les taux de d'apparition des ennemis doit être égal à 100!!!
-    int spawnRateEnnemi1 = 60;
+    int spawnRateEnnemi1 = 70;
     int spawnRateEnnemi2 = 30;
-    int spawnRateBoss = 10;
+    
 
     void EnemySpawn1(Scene* scene);
     void EnemySpawn2(Scene* scene);
@@ -23,3 +25,25 @@ public:
     std::chrono::high_resolution_clock::time_point CheckIfIsTimeToEnemySpawn(Scene* scene, std::chrono::high_resolution_clock::time_point time_for_enemy_spawn);
 };
 
+
+
+class Vague {
+private:
+    int nbVagueDebut; // Numéro de la première vague concernée
+    int nbVagueFin;   // Numéro de la dernière vague concernée
+    int newVaguemaxEnnemi;
+    double newCooldown;
+    double newCooldownBTWwaves;
+    
+
+public:
+    Vague(int debut, int fin, int newMax, double newCooldownEnnemi, double newCooldownWaves) : nbVagueDebut(debut), nbVagueFin(fin), newVaguemaxEnnemi(newMax), newCooldown(newCooldownEnnemi), newCooldownBTWwaves(newCooldownWaves) {}
+
+    void AppliquerVague(int& vaguemaxEnnemi,double& cooldown, double& cooldownBTWwaves, int nbVague) {
+        if (nbVague >= nbVagueDebut && nbVague <= nbVagueFin) {
+            vaguemaxEnnemi = newVaguemaxEnnemi;
+            cooldown = newCooldown;
+            cooldownBTWwaves = newCooldownBTWwaves;
+        }
+    }
+};
--- GameObjectLib/src/EnemySpawn.cpp
+++ GameObjectLib/src/EnemySpawn.cpp
@@ -24,14 +24,17 @@ void EnemySpawn::EnemySpawn1(Scene* scene)
 }
 
 
+
+
 std::chrono::high_resolution_clock::time_point EnemySpawn::CheckIfIsTimeToEnemySpawn(Scene* scene, std::chrono::high_resolution_clock::time_point time_for_enemy_spawn)
 
 {
-	auto duration = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - time_for_enemy_spawn);
-	double cooldown = 1.2;
 
+
+	auto duration = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - time_for_enemy_spawn);
 	
 
+	
 
 	std::random_device rd;  // Initialise le générateur de nombres aléatoires
 	std::mt19937 rng(rd()); // Utilise le générateur mersenne_twister_engine
@@ -39,42 +42,59 @@ std::chrono::high_resolution_clock::time_point EnemySpawn::CheckIfIsTimeToEnemyS
 	// Génère un nombre aléatoire entre 1 et 100
 	int randomEnemySpawn = std::uniform_int_distribution<int>(1, 100)(rng);
 
-	if (nbVague != nbVagueMax)
-	{
-		if (duration.count() >= cooldown && nbEnnemiVague < vaguemaxEnnemi)
-		{
-			if (randomEnemySpawn <= spawnRateEnnemi1)
+	//Permet de modif une ou plusieurs vague pour quelles aient le même nombre d'ennemi et le même cooldown entre le changement de vagues
+	Vague modifVague1(3, 4, 5, 3.0,  12.0);
+
+	Vague modifVague2(6, 6, 13, 0.5, 5.0);
+
+	for (int vaguemodif = nbVague; vaguemodif <= nbVagueMax; ++vaguemodif) {
+		modifVague1.AppliquerVague(vaguemaxEnnemi, cooldown, cooldownBTWwaves, nbVague);
+
+		for (int vaguemodif = nbVague; vaguemodif <= nbVagueMax; ++vaguemodif) {
+			modifVague2.AppliquerVague(vaguemaxEnnemi, cooldown, cooldownBTWwaves, nbVague);
+
+			if (nbVague != nbVagueMax)
 			{
-				EnemySpawn1(scene);
-				nbEnnemiVague++;
-			}
-			else if (randomEnemySpawn > spawnRateEnnemi1 && randomEnemySpawn <= spawnRateEnnemi1 + spawnRateEnnemi2)
-			{
-				EnemySpawn2(scene);
-				nbEnnemiVague++;
-			}
-			if (randomEnemySpawn > spawnRateEnnemi1 + spawnRateEnnemi2 && randomEnemySpawn <= spawnRateEnnemi1 + spawnRateEnnemi2 + spawnRateBoss)
-			{
-				BossSpawn(scene);
-				nbEnnemiVague++;
+				if (duration.count() >= cooldown && nbEnnemiVague < vaguemaxEnnemi)
+				{
+					if (nbVague % 3 == 0 && verifVagueBoss == true)
+					{
+						BossSpawn(scene);
+						nbEnnemiVague++;
+						verifVagueBoss = false;
+					}
+					else if (randomEnemySpawn <= spawnRateEnnemi1)
+					{
+						EnemySpawn1(scene);
+						nbEnnemiVague++;
+					}
+					else if (randomEnemySpawn > spawnRateEnnemi1 && randomEnemySpawn <= spawnRateEnnemi1 + spawnRateEnnemi2)
+					{
+						EnemySpawn2(scene);
+						nbEnnemiVague++;
+					}
+					
+
+
+					std::cout << "Ennemis : " << nbEnnemiVague << std::endl;
+					std::cout << "Max ennemis : " << vaguemaxEnnemi << std::endl;
+					std::cout << "Vagues : " << nbVague << std::endl;
+
+
+					time_for_enemy_spawn = std::chrono::high_resolution_clock::now();
+				}
+				else if (nbEnnemiVague >= vaguemaxEnnemi && duration.count() >= cooldownBTWwaves)
+				{
+					nbEnnemiVague = 0;
+					vaguemaxEnnemi = vaguemaxEnnemi * coeffMultiplicateurEnnemi;
+					nbVague++;
+					verifVagueBoss = true;
+				}
 			}
 
-			std::cout << "Ennemis : " << nbEnnemiVague << std::endl;
-			std::cout << "Max ennemis : " << vaguemaxEnnemi << std::endl;
-			std::cout << "Vagues : " << nbVague << std::endl;
-
-
-			time_for_enemy_spawn = std::chrono::high_resolution_clock::now();
-		}
-		else if (nbEnnemiVague >= vaguemaxEnnemi && duration.count() >= cooldownBTWwaves)
-		{
-			nbEnnemiVague = 0;
-			vaguemaxEnnemi = vaguemaxEnnemi * coeffMultiplicateurEnnemi;
-			nbVague++;
+			return time_for_enemy_spawn;
 		}
 	}
-
-	return time_for_enemy_spawn;
 }
 
 void EnemySpawn::EnemySpawn2(Scene* scene)
