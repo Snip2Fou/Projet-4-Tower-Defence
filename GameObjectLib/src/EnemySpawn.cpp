61d879e07f78482292bcc6d2f9f6d9e1c4a0c16d
unknown <agathelier@gamingcampus.local>
2023-11-09 10:26:38 +0100

    Erreur bizare

--- GameObjectLib/src/EnemySpawn.cpp
+++ GameObjectLib/src/EnemySpawn.cpp
@@ -24,7 +24,34 @@ void EnemySpawn::EnemySpawn1(Scene* scene)
 }
 
 
-std::chrono::high_resolution_clock::time_point Vague::CheckIfIsTimeToEnemySpawn(Scene* scene, std::chrono::high_resolution_clock::time_point time_for_enemy_spawn, int *vague)
+std::chrono::high_resolution_clock::time_point EnemySpawn::CheckIfIsTimeToEnemySpawn(Scene* scene, std::chrono::high_resolution_clock::time_point time_for_enemy_spawn)
+
+{
+	auto duration = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - time_for_enemy_spawn);
+	double cooldown = 1.2;
+
+	if (duration.count() >= cooldown && nbEnnemiVague < vaguemaxEnnemi)
+	{
+		nbEnnemiVague++;
+		std::cout << nbEnnemiVague << std::endl;
+		std::cout << vaguemaxEnnemi << std::endl;
+		std::cout << nbVague << std::endl;
+
+		EnemySpawn1(scene);
+		EnemySpawn2(scene);
+		BossSpawn(scene);
+
+		time_for_enemy_spawn = std::chrono::high_resolution_clock::now();
+	}
+	else if (nbEnnemiVague >= vaguemaxEnnemi)
+	{
+		nbEnnemiVague = 0;
+		vaguemaxEnnemi = vaguemaxEnnemi * 1.25;
+		nbVague++;
+	}
+
+	return time_for_enemy_spawn;
+}
 
 void EnemySpawn::EnemySpawn2(Scene* scene)
 {
@@ -56,86 +83,3 @@ void EnemySpawn::BossSpawn(Scene* scene)
 	GameObject* enemy = scene->CreateEnemy(name, spawn_pos, hp, speed, damage, "texture_boss"); 
 }
 
-std::chrono::high_resolution_clock::time_point EnemySpawn::CheckIfIsTimeToEnemySpawn(Scene* scene, std::chrono::high_resolution_clock::time_point time_for_enemy_spawn)
-
-{
-	auto duration = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - time_for_enemy_spawn);
-	double cooldown = 1.2;
-
-
-
-
-
-	if (duration.count() >= cooldown && nbEnnemiVague < vaguemaxEnnemi)
-	{
-
-		nbEnnemiVague++;
-		std::cout << nbEnnemiVague << std::endl;
-		std::cout << vaguemaxEnnemi << std::endl;
-		std::cout << nbVague << std::endl;
-
-		EnemySpawn(scene);
-
-		EnemySpawn1(scene);
-		EnemySpawn2(scene);
-		BossSpawn(scene);
-
-		time_for_enemy_spawn = std::chrono::high_resolution_clock::now();
-		
-	}
-	else if(nbEnnemiVague >= vaguemaxEnnemi)
-	{
-		nbEnnemiVague = 0;
-		vaguemaxEnnemi = vaguemaxEnnemi*1.25;
-		nbVague++;
-
-	}
-
-
-	/*if (*vague == 1)
-	{
-		if (duration.count() >= cooldown && nbEnnemiVague1 <= vague1maxEnnemi)
-			{
-				nbEnnemiVague1++;
-				std::cout << nbEnnemiVague1 << std::endl;
-				EnemySpawn(scene);
-				time_for_enemy_spawn = std::chrono::high_resolution_clock::now();
-			}
-		else
-		{
-			vague++;
-		}
-	}
-
-	if (*vague == 2)
-	{
-		if (duration.count() >= cooldown && nbEnnemiVague1 == vague1maxEnnemi  && nbEnnemiVague2 <= vague2maxEnnemi)
-		{
-			nbEnnemiVague2++;
-			std::cout << nbEnnemiVague2 << std::endl;
-			EnemySpawn(scene);
-			time_for_enemy_spawn = std::chrono::high_resolution_clock::now();
-		}
-		else
-		{
-			vague++;
-		}
-	}
-
-
-	if (*vague == 3)
-	{
-		if (duration.count() >= cooldown && nbEnnemiVague2 == vague2maxEnnemi && nbEnnemiVague3 <= vague3maxEnnemi)
-		{
-			nbEnnemiVague3++;
-			std::cout << nbEnnemiVague3 << std::endl;
-			EnemySpawn(scene);
-			time_for_enemy_spawn = std::chrono::high_resolution_clock::now();
-		}
-		else
-		{
-			vague++;
-		}
-	}*/
-	return time_for_enemy_spawn;
-}
